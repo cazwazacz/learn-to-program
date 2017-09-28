6ee8fdc00dc6edf4a94d210c13ed9020aaada625
Allan Wazacz <cazwazacz@gmail.com>
2017-09-28 18:21:09 +0100

    full name greeting excercise

---
+++ chap05/fullnamegreeting.rb
@@ -0,0 +1,8 @@
+puts 'What\'s your first name?'
+first = gets.chomp
+puts 'Middle name?'
+middle = gets.chomp
+puts 'Last?'
+last = gets.chomp
+
+puts 'Hello, ' + first + ' ' + middle + ' ' + last + '.'
