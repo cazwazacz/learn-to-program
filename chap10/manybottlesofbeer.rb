d190757dc093f8bdc680dd57ea550af69857353f
Allan Wazacz <cazwazacz@gmail.com>
2017-10-09 18:52:07 +0100

    bottles

--- chap10/manybottlesofbeer.rb
+++ chap10/manybottlesofbeer.rb
@@ -98,7 +98,7 @@ end
 num_at_start = 2000
 num_now = num_at_start
 while num_now > 2
-  puts english_number(num_now).capitalize + ' bottles of beer on the wall, ' + english_number(num_now) + 'bottles of beer!'
+  puts english_number(num_now).capitalize + ' bottles of beer on the wall, ' + english_number(num_now) + ' bottles of beer!'
   num_now = num_now - 1
   puts 'Take one down, pass it around ' + english_number(num_now) + ' bottles of beer on the wall!'
 end
